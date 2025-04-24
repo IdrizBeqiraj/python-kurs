8bd81b13af6d413d3c82a347669d4b4e3082fe76
IdrizBeqiraj <163838691+IdrizBeqiraj@users.noreply.github.com>
2025-04-24 20:07:47 +0200

    Initial commit

---
+++ mian.py
@@ -0,0 +1,25 @@
+print("Hello Idriz Beqiraj")
+
+temperature = 37.5
+
+print(type(temperature))
+
+emri = 'Kosova'
+
+print(emri, temperature)
+
+x = 3
+y = 4
+
+print(y - x)
+print(y + x)
+print(y * x)
+print(y / x)
+
+Shteti = 'kosova'
+Shteti2 = 'republik'
+
+print(Shteti + Shteti2)
+
+listanxenseve = ['Idriz', 'Beqiraj', 'Youtube']
+print(listanxenseve[2])
