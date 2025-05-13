35989af92ef952e9bdb8e583eaec966e00255342
IdrizBeqiraj <163838691+IdrizBeqiraj@users.noreply.github.com>
2025-05-13 17:48:14 +0200

    modulee4

--- module4/WhileLopp.py
+++ module4/WhileLopp.py
@@ -9,3 +9,11 @@ shuma = 0
 for numri in range(1, 101):
     shuma += numri
 print("Shuma e numrave nga 1 deri në 100 është:", shuma)
+
+#variablat e interimit
+i = 0
+shume = 0
+while i<=100:
+    shume=shume+i
+    i=i+1
+print("Shuma e numrave eshte",shume )
