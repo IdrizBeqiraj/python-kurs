5b12a08f4477a4715dd6952759fae3cdc2696f70
IdrizBeqiraj <idriz.beqiraj@gmail.com>
2025-06-03 17:43:55 +0200

    9
    9

---
+++ Module9/InBuiltFunction.py
@@ -0,0 +1,6 @@
+def mbledhje(x, y):
+    return x + y
+
+def operation(x, y):
+    return str(x) + str(y)
+
---
+++ Module9/ibuilt_polimorphysm.py
@@ -0,0 +1,23 @@
+string_length = len ("Idriz Beqiraj")
+list_length = len ([21, 23,21, 801, 1])
+touple_length = len ((7, 4, 6, 9, 22))
+
+
+print(string_length)
+print(string_length)
+print(touple_length)
+
+#detyra e dyt
+sum_of_list = ([4,3,1,5])
+sum_of_tuple = (20, 15, 10)
+
+print(sum_of_list)
+print(sum_of_tuple)
+
+#detyra e tret
+print(5 + 10)
+print("Hello " + "World!")
+print([1, 2] + [3, 4])
+
+print(max(1, 3, 2))
+print(max("a", "z", "m"))
