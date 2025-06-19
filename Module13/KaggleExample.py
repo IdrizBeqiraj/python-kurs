165f86b0deeb90ba81fe76b717b82861dcd14596
IdrizBeqiraj <idriz.beqiraj@gmail.com>
2025-06-19 17:49:05 +0200

    13

--- Module13/KaggleExample.py
+++ Module13/KaggleExample.py
@@ -17,3 +17,4 @@ print(null_count)
 
 #rwmoving the duplicates
 df.drop_duplicates(keep="first",inplace=True)
+df["Population - 2023"]=df["Population - 2023"].apply(lambda x: float(x.replace(",","")))
